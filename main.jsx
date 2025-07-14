40aa2a56e4f6e50e30e611f38d69044f76bc846e
Evaniaa <olajumokeeva@gmail.com>
2025-07-14 09:47:34 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/index.jsx';
-import ThankYou from './pages/thank-you.jsx';
+import Home from './index.jsx';
+import ThankYou from './thank-you.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
